7b1a57d3ab1aa0f7acfb2ce66020a6b16a0db7e3
AzizullahCodes <azizullahcodes@gmail.com>
2025-05-21 10:02:46 +0500

    Initial commit

---
+++ ap.js
@@ -0,0 +1,172 @@
+// 🔢 Array Practice Questions (No Loops or Methods)
+
+//  1.Create an array with 5 of your favorite fruits. Print the first and last fruit.
+// var fruits = ['apple','mango','banana','grapes','orange'];
+// alert('The first fruit is : ' + fruits[0]);
+// alert('The last fruits is fruit : ' + fruits[4]);
+
+//2. Create an array with 3 numbers. Find and print the sum of the first and second numbers.
+//  var array = [2,8,5];
+//  var firstNumber = array[0];
+//  var lastNumber  = array[2];
+//  var sum         = firstNumber + lastNumber;
+//  alert('The sum of first and last number is ' + sum);
+
+// 3.Make an array that stores the names of 4 countries. Change the second country to "Pakistan".
+        // var array = ['saudia','malaysia','usa','uae'];
+        // var country1 = array[1];
+        // console.log(country1);
+        // country1 = 'pakistan';
+        // console.log(country1);
+        // var newarray = ['saudia',country1,'usa','uae'];
+        // console.log(newarray);
+
+        // 4.You have an array ["red", "green", "blue"]. Store it in a variable and print each color one by one using individual index. 
+                // var colors = ['red','green','blue'];
+                // alert(colors[0]);
+                // alert(colors[1]);
+                // alert(colors[2]); 
+                // 5.Create two arrays – one for car brands and one for bike brands. Print the second car and third bike.
+                        // var bike = ['honda','metro','china','local','imported'];
+                        // var cars = ['xli','civic','alto','mehran',bike];
+                        // console.log(cars);
+                        // alert('The second car is : ' + cars[1] + ' and third bike is ' + cars[4][2] );
+
+                        // 6. Make an array of 6 numbers. Swap the first and last number in the array manually (use indexing, not any method).
+                        // var numbers = [3,9,2,7,1,4];
+                        // var firstNumber = numbers[0];
+                        // console.log(firstNumber); 
+                        // var lastNumber = numbers[5];
+                        // console.log(lastNumber);
+                        // firstNumber = lastNumber;
+                        // console.log(firstNumber);
+                        // lastNumber = numbers[0];
+                        // console.log(lastNumber);
+
+                        // var newArray = [firstNumber,9,2,7,1,lastNumber];
+                        // console.log(newArray); 
+                        //  7.Make an array with student names. Print the total number of students using the .length property only.
+                        //  var studentNames = ['ali','hadi','muhib','qadir'];
+                        //  var totalLength  = studentNames.length;
+                        //  console.log(totalLength); 
+
+                        // 8. Declare an empty array and then manually assign 3 values using index numbers. Print all values.
+                        //  var array = [];
+                        //  var value1 = 'apple';
+                        //  var value2 = 'orange';
+                        //  var value3 = 'banan'; 
+                        //  var newArray = [value1,value2,value3];
+                        //  console.log(newArray);
+
+                        // 9. You have the array ["apple", "banana", "cherry", "date"]. Replace "banana" with "mango" using index.
+                                // var array = ['apple','banana','cherry','date'];
+                                // var replace = array[1];
+                                // console.log(replace);
+                                // replace = 'mango';
+                                // var newArray = ['apple',replace,'cherry','date']; 
+                                // console.log(newArray);
+
+                        // 10.Create an array of 4 boolean values. Change the third one to true.
+                                // var array = [true,true,false,false];
+                                // var thirdBoolean = array[2];
+                                // console.log(thirdBoolean); 
+                                // thirdBoolean = true;
+                                // var newArray = [true,true,thirdBoolean,false];
+                                // console.log(newArray);
+
+                                //11.Create an array of 5 animals. Print the third animal using its index.
+                                        // var animals = ['goat','sheep','cow','buffalo','dog']; 
+                                        // var thirdAnimal = animals[2];
+                                        // console.log(thirdAnimal); 
+
+                                        
+// 12.Create an array of 5 students' marks: [78, 85, 92, 67, 88].
+//  // Find the average manually (add each and divide by 5).
+        // var marks = [78,56,98,78,34];
+        // var student1Marks = marks[0];
+        // var student2Marks = marks[1]; 
+        // var student3Marks = marks[2];
+        // var student4Marks = marks[3];
+        // var student5Marks = marks[4];
+        // var totalMarks = student1Marks + student2Marks + student3Marks + student4Marks + student5Marks; 
+        // console.log(totalMarks); 
+        // var averageMarks = totalMarks / 5;
+        // console.log(averageMarks);
+
+// question=13        You have two arrays:
+// let boys = ["Ali", "Ahmed", "Zain"]
+// let girls = ["Sara", "Ayesha", "Noor"]
+
+// Make a new variable that holds the 1st boy and 2nd girl together like "Ali & Ayesha".
+
+        // var  boys = ["Ali", "Ahmed", "Zain"];
+        //  var  girls = ["Sara", "Ayesha", "Noor"];
+        //  var firstBoy = boys[0]; 
+        //  var secondGirls = girls[1];
+        //  var newArray = [firstBoy,secondGirls];
+        //  console.log(newArray); 
+
+// question=14        Declare an array of 4 temperatures: [30, 35, 33, 31].
+
+// Find the difference between the highest and lowest manually.
+        // var temperatures = [30,35,33,31];
+        // var lowest = temperatures[0];
+        // var highest = temperatures[1];
+        // var difference = highest - lowest;
+        // console.log(difference);
+
+// question=15        You have this array:
+// let data = ["apple", 10, true, "banana", false, 25]
+
+// Store all strings (apple, banana) into new variables using only indexing.
+        // var  data = ["apple", 10, true, "banana", false, 25]; 
+        // var string1 = data[0]; 
+        // var string2 = data[3]; 
+        // var newArray = [string1,string2];
+        // console.log(newArray);
+
+// question=16         You have this array:
+// let colors = ["red", "blue", "green", "yellow", "purple"]
+
+// Manually reverse the entire array using indexing (no .reverse()).
+        // var colors = ["red", "blue", "green", "yellow", "purple"]; 
+        // var color1 = colors[0]; 
+        // var color2 = colors[1]; 
+        // var color3 = colors[2]; 
+        // var color4 = colors[3]; 
+        // var color5 = colors[4]; 
+        // var newArray = [color5,color4,color3,color2,color1];
+        // console.log(newArray); 
+
+// question = 17        Given this array:
+// let mix = ["pen", 12, "notebook", 5, true, "eraser"]
+
+// Print only the items that are not numbers (use indexing, no typeof).
+
+// var mix = ["pen", 12, "notebook", 5, true, "eraser"]; 
+// alert(mix[0] + '\n' + mix[2] + '\n' + mix[4] + ' and ' + '\n' + mix[5] + ' are not numbers' );
+
+// question= 18 You are given:
+// let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+// Create a new array weekend using just the 6th and 7th values from days.
+ 
+        // var  days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];  
+        // var weekend = [days[5],days[6]] ; 
+        // console.log(weekend); 
+
+        // question = 19; You have three arrays:
+// let num1 = [10, 20], let num2 = [30, 40], let num3 = [50, 60]
+
+// Manually calculate the sum of the second value in each array.
+        // var  num1 = [10, 20]; 
+        // var  num2 = [30, 40]; 
+        // var  num3 = [50, 60]; 
+        // var calculation = num1[1] + num2[1] + num3[1]; 
+        // console.log(calculation);
+
+        //question = 20' You have an array of mixed values:
+// let items = ["apple", 45, true, "banana", false, 33, "grape"]
+
+// Create 3 new variables: one that holds only the strings, one for numbers, and one for booleans (all picked manually using index).
+var items = ["apple", 45, true, "banana", false, 33, "grape"]; 
